5003679c0262c7170e17b1f5d486876e2eee0284
jhonvelasque <jhon.velasque@unmsm.edu.pe>
2023-08-07 00:17:18 -0500

    jhon

---
+++ src/saludpacifico/AdiosMundo.java
@@ -0,0 +1,21 @@
+package saludpacifico;
+
+/**
+ * esta es la clase que se creea por defecto cuando creamos una clase
+ */
+public class AdiosMundo {
+    // esto es un comentario
+    /*
+    -- este comentario es para nosotros
+    recomendacion esquematizar paso por paso lo que tenemos que hacer
+    */
+
+    /**
+     * esta aplicación es la inicial
+     * @param args--argumentas de la entrada para la aplicación
+     */
+    public static  void main (String[] args ){
+        System.out.println("Adios Mundo Cruel");
+    }
+
+}
---
+++ src/saludpacifico/Tipos_Datos.java
@@ -0,0 +1,27 @@
+package saludpacifico;
+// link de curos https://youtu.be/lj1VPo0lqkA
+public class Tipos_Datos {
+    public static void main(String[] args) {
+        //Numericos
+        //los rosados son tipos de datos primitivos
+            //1.1entero
+        byte varible=3;
+        int varible1=5;
+        long varible2=100;
+        System.out.println(varible+2);
+        System.out.println(varible1+2);
+        System.out.println(varible2+2);
+            //1.2decimales
+        float varible3 =5.5f;
+        double varible4 =10.5d;
+        //booleanos
+        boolean varible5 =false;
+
+        //textos
+        char varible6 ='a';
+        // string no es un tipo de dato primitivo es un Objeto
+        String varible7="esto es un string ";
+        System.out.println(varible);
+
+    }
+}
